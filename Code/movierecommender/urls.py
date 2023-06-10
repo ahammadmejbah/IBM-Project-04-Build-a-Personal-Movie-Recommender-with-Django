45f4090a6973d0301a46c399085a06e669f02375
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2023-06-10 06:40:36 -0400

    Updates

---
+++ Code/movierecommender/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # route is a string contains a URL pattern
+]
---
+++ Code/movierecommender/views.py
@@ -0,0 +1,6 @@
+from . import views
+from .models import Movie
+from django.shortcuts import render
+
+# HINT: Create a view to provide movie recommendations list for the HTML template
+
